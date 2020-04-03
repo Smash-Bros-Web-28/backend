8b90ad6759eb96639c433b5a3f7b5f6f5b02a2fb
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-02 22:00:53 -0600

    created users table

---
+++ data/migrations/20200402212623_users.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl =>{
+        tbl.increments();
+        tbl.string('firstName')
+            .notNullable();
+        tbl.string('lastName')
+            .notNullable();
+        tbl.string('gamerTag')
+            .notNullable()
+            .unique();
+        tbl.string('email')
+            .notNullable()
+            .unique()
+        tbl.string('password')
+            .notNullable()
+        tbl.integer('favorite_character_id')
+
+  })
+};
+
+exports.down = function(knex) {
+  
+};
