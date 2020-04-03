1445658ba5c0cf51010f3c07f9b8cec96fb6b5be
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-02 23:02:01 -0600

    users model created

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,5 +1,24 @@
 const db = require('../data/dbConfig.js')
 
 module.exports = {
-    
+    findAllUsers,
+    findUserByName,
+    addUser,
+    removeUser
+}
+
+function findAllUsers() {
+    return db('users')
+}
+
+function addUser(creds) {
+    return db('users').insert(creds, db('users').where(creds.username))
+}
+
+function findUserByName(username) {
+    return db('users').where(username)
+}
+
+function removeUser(id) {
+    return db('users').where({ id }).del()
 }
